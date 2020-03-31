e5384dfac0537bf52c23cff035b0c3ec9cad4115
danas <danas@sk8.pl>
2020-03-31 14:54:30 +0200

    1st commit

---
+++ script.js
@@ -0,0 +1,49 @@
+
+function MojKomponent() {
+  return (
+    <div>
+      <h1>Witaj, świecie!</h1>
+      <h2>Jestem ambitna/y, więc przerabiam kurs React.js!</h2>
+    </div>
+  );
+}
+    ReactDOM.render(
+      <MojKomponent />,
+      document.getElementById('app')
+    );
+/*<header className="ui fixed menu">
+  <nav className="ui container">
+    <a href="#" className="header item">
+      <img className="logo" src="https://typeofweb.com/wp-content/uploads/2017/08/cropped-typeofweb_logo-04-white-smaller-1-e1504359870362.png" />
+      Lista kontaktów
+    </a>
+    <div className="header item">
+      <button className="ui button">Dodaj</button>
+    </div>
+  </nav>
+</header>
+<main className="ui main text container">
+  <ul className="ui relaxed divided list selection">
+    <li className="item">
+      <img src="https://api.adorable.io/avatars/55/typeofweb1.png" className="ui mini rounded image" />
+      <div className="content">
+        <h4 className="header">Lena</h4>
+        <div className="description">JavaScript Developer</div>
+      </div>
+    </li>
+    <li className="item">
+      <img src="https://api.adorable.io/avatars/55/typeofweb2.png" className="ui mini rounded image" />
+      <div className="content">
+        <h4 className="header">Brian</h4>
+        <div className="description">Human Resources</div>
+      </div>
+    </li>
+    <li className="item">
+      <img src="https://api.adorable.io/avatars/55/typeofweb3.png" className="ui mini rounded image" />
+      <div className="content">
+        <h4 className="header">Rick</h4>
+        <div className="description">QA</div>
+      </div>
+    </li>
+  </ul>
+</main> */
