562622c14814e6ffa76f8b6fbda5f1e3a77cec5e
danas <mateuszdanek@gmail.com>
2020-04-04 13:20:43 +0200

    podzial na foldery

---
+++ 01-intro/script.js
@@ -0,0 +1,52 @@
+// function MojKomponent() {
+//   return (
+//     <div>
+//       <h1>Witaj, świecie!</h1>
+//       <h2>Jestem ambitna/y, więc przerabiam kurs React.js!</h2>
+//     </div>
+//   );
+// }
+
+// ReactDOM.render(
+//   <MojKomponent />,
+//   document.getElementById('app')
+// );
+
+// document.getElementById("app").innerHTML = "no coorva...";
+
+<header class="ui fixed menu">
+  <nav class="ui container">
+    <a href="#" class="header item">
+      <img class="logo" src="https://typeofweb.com/wp-content/uploads/2017/08/cropped-typeofweb_logo-04-white-smaller-1-e1504359870362.png" />
+      Lista kontaktów
+    </a>
+    <div class="header item">
+      <button class="ui button">Dodaj</button>
+    </div>
+  </nav>
+</header>
+<main class="ui main text container">
+  <ul class="ui relaxed divided list selection">
+    <li class="item">
+      <img src="https://api.adorable.io/avatars/55/typeofweb1.png" class="ui mini rounded image" />
+      <div class="content">
+        <h4 class="header">Lena</h4>
+        <div class="description">JavaScript Developer</div>
+      </div>
+    </li>
+    <li class="item">
+      <img src="https://api.adorable.io/avatars/55/typeofweb2.png" class="ui mini rounded image" />
+      <div class="content">
+        <h4 class="header">Brian</h4>
+        <div class="description">Human Resources</div>
+      </div>
+    </li>
+    <li class="item">
+      <img src="https://api.adorable.io/avatars/55/typeofweb3.png" class="ui mini rounded image" />
+      <div class="content">
+        <h4 class="header">Rick</h4>
+        <div class="description">QA</div>
+      </div>
+    </li>
+  </ul>
+</main>
